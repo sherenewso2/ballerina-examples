99d3fc5358aaac2bfea66ebfd2aa95209c5550ec
sherene <sherene@wso2.com>
2018-04-16 15:14:44 +0530

    Update https-server-connector.client.sh

--- examples/https-server-connector/https-server-connector.client.sh
+++ examples/https-server-connector/https-server-connector.client.sh
@@ -1,3 +1,3 @@
-#For the demonstration purpose curl -k command will be used to invoke the service.
+#Run this curl -k command to invoke the service.
 $ curl -k https://localhost:9095/hello
 Successful
